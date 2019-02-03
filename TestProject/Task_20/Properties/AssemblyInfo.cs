949229f60c33d0154386466ab04ae298229f781c
GlebGusev <44800084+GlebGusev@users.noreply.github.com>
2019-02-04 01:39:04 +0300

    first commit

---
+++ TestProject/Task_20/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Task_20")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task_20")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c15301a5-599a-4856-b5f6-f1c85d00d445")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestProject/Task_20/Tests/Task_20.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Task_20.Tests
+{
+    [TestFixture]
+    public class Task_20
+    {
+        private IWebDriver driver;
+        private WebDriverWait _wait;
+        private Locators.Locators _locators;
+        private readonly Uri _startPage = new Uri("https://www.tut.by/");
+        private readonly string _username = "seleniumtests@tut.by";
+        private readonly string _password = "123456789zxcvbn";
+
+        [SetUp]
+        public void TestSetup()
+        {
+            driver = new ChromeDriver(chromeDriverDirectory: Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Drivers\"));
+            driver.Manage().Window.Maximize();
+            _locators = new Locators.Locators();
+        }
+
+        [Test]
+        public void LoginTutBy_CorrectCredentials_Successfull()
+        {
+            //Open start page
+            driver.Navigate().GoToUrl(_startPage);
+
+            //Enter credentials
+            driver.FindElement(_locators.enterLink).Click();
+            
+            var userNameInput = driver.FindElement(_locators.userNameInput);
+            userNameInput.Clear();
+            userNameInput.SendKeys(_username);
+
+            var passwordInput = driver.FindElement(_locators.passwordInput);
+            passwordInput.Clear();
+            passwordInput.SendKeys(_password);
+
+            //Assert forgot password link displays
+            Assert.True(driver.FindElement(_locators.forgotPasswordLink).Displayed, "Forgot password link is not displayed");
+
+            //Login
+            driver.FindElement(_locators.enterButton).Click();
+
+            //Validate user logged in
+            driver.FindElement(_locators.loggedInSpan).Click();
+
+            Assert.AreEqual("Личный кабинет", driver.FindElement(_locators.personalRoomText).Text);
+        }
+
+        [TearDown]
+        public void TestCleanup()
+        {
+            driver.FindElement(_locators.logOutLink).Click();
+            driver.Quit();
+        }
+    }
+}
